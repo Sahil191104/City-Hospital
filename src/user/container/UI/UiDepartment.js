4fb77d05fc417652ef56e10678cc68085366c4ce
Sahil191104 <sahilrupareliya95@gmail.com>
2023-08-01 23:02:18 +0530

    01-08-2023(Redux-Department)

--- src/user/container/UI/UiDepartment.js
+++ src/user/container/UI/UiDepartment.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card, CardBody, CardSubtitle, CardTitle, CardText, Button } from 'reactstrap';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import Titel from './Title/Titel';
+import Htag from './H1toH6/Htag';
 
 function UiDepartment({ Udata, OnClick, Onicon }) {
     console.log(Udata);
@@ -19,14 +21,51 @@ function UiDepartment({ Udata, OnClick, Onicon }) {
         console.log(heartCounter);
     }
 
-    let heartdata;
-
-    const onicondata = (id) => {
-        heartdata = localdataheart.find((v) => v.pid === id)
-    }
-
     return (
         <>
+            {/* <section id="departments" className="departments">
+                <div className="container">
+                    <div className="section-title">
+                        <Htag name="h2tag1">Departments</Htag>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-3">
+                            <ul className="nav nav-tabs flex-column">
+                                {
+                                    Udata.map((v) => {
+                                        return (
+                                            <li className="nav-item">
+                                                <a className="nav-link active show" data-bs-toggle="tab" href={v.pid}>{v.name}</a>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
+                        <div className="col-lg-9 mt-4 mt-lg-0">
+                            <div className="tab-content">
+                                {
+                                    Udata.map((v) => {
+                                        return (
+                                            <div className="tab-pane active show" id={v.pid}>
+                                                <div className="row">
+                                                    <div className="col-lg-8 details order-2 order-lg-1">
+                                                        <h3>{v.name}</h3>
+                                                        <Titel className="fst-italic">{v.desc}</Titel>
+                                                    </div>
+                                                    <div className="col-lg-4 text-center order-1 order-lg-2">
+                                                        <img src="../assets/img/departments-1.jpg" alt className="img-fluid" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section> */}
             {
                 Udata.map((v) => {
                     return (
